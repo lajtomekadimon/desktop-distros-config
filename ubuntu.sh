43670dff2add31b331b1c4b20b8525090fb0e6a9
Lajto <85128868+lajtomekadimon@users.noreply.github.com>
2024-05-02 20:29:28 +0200

    Update Ubuntu 22.04 to Kubuntu 24.04 using Sway.

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,5 @@
-# LAST UBUNTU VERSION TESTED: 22.04 LTS
-# Desktop environment: GNOME
+# LAST UBUNTU VERSION TESTED: 24.04 LTS (KUBUNTU)
+# Desktop environment: Sway (also KDE)
 
 ###############################################################################
 #                                   BASICS                                    #
@@ -9,20 +9,6 @@
 
 sudo apt install git make
 
-# Use Vanilla GNOME (choose Xorg GNOME in GDM after reboot)
-sudo apt install -y gnome-session
-#sudo update-alternatives --config gdm3.css
-
-# Remove unwanted software
-sudo apt remove -y gnome-mines gnome-mahjongg shotwell \
-gnome-sudoku gnome-todo aisleriot totem firefox
-
-# Firefox ESR
-sudo apt install -y software-properties-common apt-transport-https
-sudo add-apt-repository -y ppa:mozillateam/ppa
-sudo apt update
-sudo apt install -y firefox-esr
-
 # REBOOT
 
 ###############################################################################
@@ -30,32 +16,23 @@ sudo apt install -y firefox-esr
 ###############################################################################
 
 # Fonts
-sudo apt install -y fonts-cantarell fonts-noto
-sudo apt install -y ttf-mscorefonts-installer
+sudo apt install fonts-cantarell fonts-noto
+sudo apt install ttf-mscorefonts-installer
 wget https://github.com/adobe-fonts/source-code-pro/archive/1.017R.zip && \
 unzip 1.017R.zip && sudo mv source-code-pro-1.017R/OTF/*.otf \
 /usr/local/share/fonts/ && fc-cache -f -v && rm 1.017R.zip && rm -Rf \
 source-code-pro-1.017R
 
 # Japanese and Chinese input
-sudo apt install -y ibus-anthy ibus-libpinyin libpinyin-utils
+sudo apt install ibus-anthy ibus-libpinyin libpinyin-utils
 
 # Install software
-sudo apt install -y p7zip unrar mpv rhythmbox gimp inkscape brasero \
-screenfetch neofetch kid3 gnome-tweaks keepassxc virtualbox linphone \
-soundconverter gparted audacity anki geogebra thunderbird gnome-clocks
-
-# Custom GNOME shortcuts
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up \
-"['<Super>Page_Up', '<Control><Alt>Up', '<Control><Alt>k']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down \
-"['<Super>Page_Down', '<Control><Alt>Down', '<Control><Alt>j']"
-
-# Clean packages
-#sudo apt autoremove
+sudo apt install p7zip unrar mpv rhythmbox gimp inkscape brasero \
+screenfetch neofetch kid3 keepassxc virtualbox \
+soundconverter gparted audacity geogebra thunderbird
 
 # NodeJS
-sudo apt install -y nodejs npm
+sudo apt install nodejs npm
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
@@ -65,16 +42,10 @@ sudo npm install -g typescript
 sudo npm install -g cordova
 
 # Android
-sudo apt install -y android-tools-adb
-sudo apt install -y openjdk-11-jdk
-sudo apt install -y openjdk-8-jdk
-sudo add-apt-repository -y ppa:maarten-fonville/android-studio
+sudo apt install android-tools-adb openjdk-11-jdk openjdk-8-jdk
+sudo add-apt-repository ppa:maarten-fonville/android-studio
 sudo apt update
-sudo apt install -y android-studio gradle android-sdk-build-tools
-
-# LaTeX
-sudo apt install -y latexmk luatex
-sudo apt install --install-recommends texlive-full
+sudo apt install android-studio gradle android-sdk-build-tools
 
 # Telegram
 wget -O telegram.tar.xz https://telegram.org/dl/desktop/linux
@@ -82,3 +53,41 @@ tar -xvf telegram.tar.xz
 rm telegram.tar.xz
 mv Telegram ~/.telegram-desktop-dir
 ~/.telegram-desktop-dir/Telegram
+
+##################
+
+sudo apt install sway swaylock swayidle swaybg waybar foot fuzzel policykit-1
+
+cp tiling-wm-config/.gtkrc-2.0 ~/.gtkrc-2.0
+mkdir -p ~/.config/gtk-3.0
+cp tiling-wm-config/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+mkdir -p ~/.config/sway
+cp tiling-wm-config/.config/sway/config ~/.config/sway/config
+mkdir -p ~/.config/waybar
+cp tiling-wm-config/.config/waybar/config ~/.config/waybar/config
+cp tiling-wm-config/.config/waybar/style.css ~/.config/waybar/style.css
+
+# TODO: DBeaver
+
+# TODO: Chrome
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
